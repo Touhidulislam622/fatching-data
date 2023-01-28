2f79727b13f077802e65120cdc80ed9ae7874dd4
Touhidulislam622 <106024626+Touhidulislam622@users.noreply.github.com>
2023-01-29 01:24:57 +0600

    Add files via upload

--- DataFeaching.js
+++ DataFeaching.js
@@ -1,30 +1,59 @@
 import React , {useState , useEffect} from 'react'
 
+
+const loadingMassage = <p> todo is loading</p>
+
 const DataFeaching = () => {
 
-    const [todos , setTodos] = useState(null)
+    const [todos , setTodos] = useState(null);
+    const [isLoading , setIsloading] = useState(true);
+    const [error , setError] = useState(null);
+
+
 
     useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/todos")
+        //  setTimeout remove kora jabe //
+       setTimeout(() => {
+        fetch("https://jsonplaceholder.typicode.com/todo")
         .then((res) =>{
-            return res.json();
+            if( !res.ok){
+                throw Error("facthing is not succesful");
+            }
+            else {
+                return res.json();
+            }
         })
         .then((data) => {
             setTodos(data);
+            setIsloading(false);
+            setError(null);
         })
+        .catch((error) => {
+            setError(error.massage);
+            setIsloading(false);
+        });
+       }, 2000)
     }, [] );
+
+    const todosElement = [
+        todos && todos.map((todo) => {
+            return <div>
+                <p>{todo.id}</p>
+                <h3>{todo.title}</h3>
+    
+            </div>
+            
+           })
+    ];
+
+    const styleError={color:"red"};
 
   return (
     <div>
         <h1>Todos</h1>
-       {todos && todos.map((todo) => {
-        return <div>
-            <p>{todo.id}</p>
-            <h3>{todo.title}</h3>
-
-        </div>
-        
-       })}
+        {error && <h2>{error}</h2> }
+        <b style={styleError}> {isLoading && loadingMassage } </b>
+        {todosElement}
     </div>
   );
 };
